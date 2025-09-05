678ee3bf9a2c8689e09707008055ca1fdd2561d0
abdelaziz-elshrkawy <abdelaziz_elshrkawy53@yahoo.com>
2025-09-05 14:46:35 +0300

    adding if not exist to create_db

--- create_db.bat
+++ create_db.bat
@@ -1,5 +1,5 @@
 @echo off
 set /p password="Enter MySQL root password: "
-mysql -u root -p%password% -e "create database expanders360_platform"
+mysql -u root -p%password% -e "create database if not exist expanders360_platform"
 mysql -u root -p%password% expanders360_platform < schema.sql
 echo Completed Successfully..
--- create_db.sh
+++ create_db.sh
@@ -2,7 +2,7 @@
 
 read -sp "Enter MySQL root password: " password
 
-mysql -u root -p"$password" -e 'create database expanders360_platform'
+mysql -u root -p"$password" -e 'create database if not exist expanders360_platform'
 mysql -u root -p"$password" expanders360_platform < schema.sql
 
 echo 'Completed Successfully..'
